c23a60561de27e8ff0ed96cded6a5d2d93ca6329
William Alcorn <willalcorn19@gmail.com>
2026-01-14 19:01:19 +0000

    changed GPT model

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ else:
 
         # Generate an answer using the OpenAI API.
         stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
+            model="gpt-4.1",
             messages=messages,
             stream=True,
         )
