2cb9f3a0ae66097f29be1f2e4a503a5c8746ffc9
William Alcorn <willalcorn19@gmail.com>
2026-02-11 18:29:29 +0000

    Lab4.A

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,15 @@ import streamlit as st
 import Labs.Lab1 as Lab1
 import Labs.Lab2 as Lab2
 import Labs.Lab3 as Lab3
+import Labs.Lab4 as Lab4
 import HW.HW1 as HW1
 import HW.HW2 as HW2
 import HW.HW3 as HW3
 
 # Set up the navigation with separate sections
 pg = st.navigation([
-    st.Page(Lab3.lab3, title="Lab 3 - Chatbot", icon="🤖", default=True),
+    st.Page(Lab4.lab4, title="Lab 4 - Chatbot using RAG", icon="🔍", default=True),
+    st.Page(Lab3.lab3, title="Lab 3 - Chatbot", icon="🤖"),
     st.Page(Lab2.lab2, title="Lab 2 - Document Summarizer", icon="📄"),
     st.Page(Lab1.lab1, title="Lab 1", icon="🔬"),
     st.Page(HW3.hw3, title="HW3 - URL Chatbot", icon="🤖"),
