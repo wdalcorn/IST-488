9609d600a2fad1d04fc4213e47d3d669ba65450d
William Alcorn <willalcorn19@gmail.com>
2026-02-18 18:38:35 +0000

    Created folders for Labs and Homeworks

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,17 +10,21 @@ import HW.HW3 as HW3
 import HW.HW4 as HW4
 
 # Set up the navigation with separate sections
-pg = st.navigation([
-    st.Page(Lab5.lab5, title="Lab 5 - Weather Chatbot", icon="🌤️", default=True),
-    st.Page(Lab4.lab4, title="Lab 4 - Chatbot using RAG", icon="🔍"),
-    st.Page(Lab3.lab3, title="Lab 3 - Chatbot", icon="🤖"),
-    st.Page(Lab2.lab2, title="Lab 2 - Document Summarizer", icon="📄"),
-    st.Page(Lab1.lab1, title="Lab 1", icon="🔬"),
-    st.Page(HW4.hw4, title = "HW4 - iSchool Chatbot", icon="👨🏻‍🏫"),
-    st.Page(HW3.hw3, title="HW3 - URL Chatbot", icon="🤖"),
-    st.Page(HW2.hw2, title="HW 2 - URL Summarizer", icon="🌐"),
-    st.Page(HW1.hw1, title="HW 1", icon="📝")
-])
+pg = st.navigation({
+    "Labs": [
+        st.Page(Lab5.lab5, title="Lab 5 - Weather Chatbot", icon="🌤️", default=True),
+        st.Page(Lab4.lab4, title="Lab 4 - Chatbot using RAG", icon="🔍"),
+        st.Page(Lab3.lab3, title="Lab 3 - Chatbot", icon="🤖"),
+        st.Page(Lab2.lab2, title="Lab 2 - Document Summarizer", icon="📄"),
+        st.Page(Lab1.lab1, title="Lab 1", icon="🔬"),
+    ],
+    "Homework": [
+        st.Page(HW4.hw4, title="HW4 - iSchool Chatbot", icon="👨🏻‍🏫"),
+        st.Page(HW3.hw3, title="HW3 - URL Chatbot", icon="🤖"),
+        st.Page(HW2.hw2, title="HW 2 - URL Summarizer", icon="🌐"),
+        st.Page(HW1.hw1, title="HW 1", icon="📝"),
+    ]
+})
 
 # Run the selected page
 pg.run()
